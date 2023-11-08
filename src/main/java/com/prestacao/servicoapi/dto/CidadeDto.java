df60139dc91e381db34f4de6a0657363fa08b368
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-11-08 23:52:29 +0100

    Imports removed

--- src/main/java/com/prestacao/servicoapi/dto/CidadeDto.java
+++ src/main/java/com/prestacao/servicoapi/dto/CidadeDto.java
@@ -1,15 +1,5 @@
 package com.prestacao.servicoapi.dto;
 
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
-import com.prestacao.servicoapi.models.Endereco;
-import com.prestacao.servicoapi.models.Provincia;
-
 import lombok.Builder;
 import lombok.Data;
 
--- src/main/java/com/prestacao/servicoapi/dto/ProvinciaDto.java
+++ src/main/java/com/prestacao/servicoapi/dto/ProvinciaDto.java
@@ -1,15 +1,5 @@
 package com.prestacao.servicoapi.dto;
 
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.prestacao.servicoapi.models.Cidade;
-
 import lombok.Builder;
 import lombok.Data;
 
